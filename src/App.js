1b094b898698183fcc7c1b3e605933a1eaaab831
shwetangi03 <113777115+shwetangi03@users.noreply.github.com>
2024-05-14 20:23:45 +0530

    Fixing the Cart problems

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Fragment, useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
+
 
 let isInitial = true;
 
@@ -14,50 +15,16 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
+  useEffect(()=>{
+    dispatch(fetchCartData)
+  },[dispatch])
+
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending...",
-          title: "sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const res = await fetch(
-        "https://expense-tracker-real-time-data-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!res.ok) {
-        throw new Error("Sending cart data failed!!");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "Success",
-          title: "Success!",
-          message: "Sent cart data successfully !!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "sending cart data failed !!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart))
   }, [cart, dispatch]);
 
   return (
